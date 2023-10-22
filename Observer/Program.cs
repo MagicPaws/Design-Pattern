a24476b736d924f548d28cdbe9469eb408a361ef
MagicPaws <19239639479@192.net>
2023-10-22 20:21:19 +0800

    观察者模式(红绿灯) 定义一种一对多的依赖关系，让多个观察者对象同时监听某一个主题对象。这个主题对象在状态发生变化时，会通知所有观察者对象，使他们能够自动更新自己。

---
+++ Observer/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace ObserverPattern
+{
+    static class Program
+    {
+        public class Light : Subject
+        {
+            public string color = "red";
+
+            public void ChangeColor()
+            {
+                switch (color)
+                {
+                    case "red":
+                        color = "green";
+                        break;
+                    case "green":
+                        color = "red";
+                        break;
+                    default:
+                        this.color = "red";
+                        break;
+                }
+                Publish();
+            }
+        }
+
+        public class Car : Observer
+        {
+            public string name;
+            public string status;
+
+            public override void Update(Subject subject)
+            {
+                switch ((subject as Light).color)
+                {
+                    case "red":
+                        status = "Stop!";
+                        break;
+                    case "green":
+                        status = "Go!";
+                        break;
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Car car0 = new Car();
+            Car car1 = new Car();
+            Car car2 = new Car();
+            car0.name = "小轿车";
+            car1.name = "小货车";
+            car2.name = "大货车";
+            Light light = new Light();
+            light.AddObserver(car0);
+            light.AddObserver(car1);
+            light.AddObserver(car2);
+
+            // 在循环之前等待一段时间
+            Thread.Sleep(5000);
+
+            while (true)
+            {
+                light.ChangeColor();
+                Console.WriteLine("灯状态为: " + light.color);
+                Console.WriteLine("小轿车状态为: " + car0.status);
+                Console.WriteLine("小货车状态为: " + car1.status);
+                Console.WriteLine("大货车状态为: " + car2.status);
+                Console.WriteLine("------------------------------------");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
---
+++ Observer/Subject.cs
@@ -0,0 +1,31 @@
+﻿namespace ObserverPattern;
+
+/* 策略模式
+ * 定义一种一对多的依赖关系，让多个观察者对象同时监听某一个主题对象。这个主题对象在状态发生变化时，会通知所有观察者对象，使他们能够自动更新自己。
+ * 红绿灯——红灯停绿灯行
+ */
+
+// 定义抽象类Subject
+public abstract class Subject
+{
+    // 定义私有变量_observers
+    private List<Observer> _observers = new List<Observer>();
+    
+
+    // 定义添加观察者方法
+    public void AddObserver(Observer observer)
+    {
+        // 将观察者添加到_observers列表中
+        this._observers.Add(observer);
+    }
+    // 定义发布方法
+    public void Publish()
+    {
+        // 遍历_observers列表
+        foreach (Observer observer in  _observers)
+        {
+            // 调用观察者的更新方法，传入当前对象
+            observer.Update(this);
+        }
+    }
+}
