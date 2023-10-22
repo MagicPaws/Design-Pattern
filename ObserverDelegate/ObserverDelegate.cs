fd907ff5dd42b1f8bb2882964f7bfec9cbb1d71c
MagicPaws <19239639479@192.net>
2023-10-22 21:13:56 +0800

    观察者模式(委托 红绿灯) 利用委托与事件，将观察者的行为与被观察的行为联系在一起

---
+++ ObserverDelegate/ObserverDelegate.cs
@@ -0,0 +1,59 @@
+﻿namespace ObserverDelegate;
+
+public delegate void ColorChanged(Light l);
+
+public class Light
+{
+    public event ColorChanged ColorChanged;
+    public string color;
+
+    public void ChangeColor()
+    {
+        switch (color)
+        {
+            case "red":
+                color = "green";
+                break;
+            case "green":
+                color = "red";
+                break;
+            default:
+                color = "red";
+                break;
+        }
+
+        ShowStatus();
+        if (ColorChanged != null)
+        {
+            ColorChanged(this);
+        }
+    }
+
+    private void ShowStatus()
+    {
+        Console.WriteLine("当前红路灯为:" + color + "");
+    }
+}
+
+public class Car
+{
+    public string name;
+    public string status;
+
+    public void Stop()
+    {
+        status = "stop";
+        ShowStatus();
+    }
+
+    public void Go()
+    {
+        status = "go";
+        ShowStatus();
+    }
+
+    private void ShowStatus()
+    {
+        Console.WriteLine("{0} 状态为 {1}", name, status);
+    }
+}
---
+++ ObserverDelegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+using ObserverDelegate;
+
+static class Program
+{
+    static void Main()
+    {
+        Car car0 = new Car();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        car0.name = "小轿车";
+        car1.name = "大轿车";
+        car2.name = "跑车";
+        List<Car> list = new List<Car>() { car0, car1, car2 };
+        Light light = new Light();
+        light.ColorChanged += (light) =>
+        {
+            if (light.color == "red")
+            {
+                foreach (var car in list)
+                {
+                    car.Stop();
+                }
+            }
+            else
+            {
+                foreach (var car in list)
+                {
+                    car.Go();
+                }
+            }
+        };
+        while (true)
+        {
+            light.ChangeColor();            
+            Thread.Sleep(5000);
+        }
+    }
+}
