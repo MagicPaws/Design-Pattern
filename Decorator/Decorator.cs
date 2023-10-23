ac355f5ff46e98984a3fb4331b6ed4a2c1e5ee3e
MagicPaws <19239639479@192.net>
2023-10-23 17:28:58 +0800

    装饰器模式 (Buff) 1.通过将一个对象包装在一个或多个装饰器中，以在运行时增加新的功能或行为 2.注意是调用装饰器的SetComponent来修改原有对象，达到动态修改

---
+++ Decorator/Decorator.cs
@@ -0,0 +1,45 @@
+﻿namespace Decorator;
+
+public abstract class Gun
+{
+    public float atk;
+    public abstract void Shoot();    
+}
+
+public class Ak : Gun
+{
+    public Ak()
+    {
+        atk = 100;
+    }
+    public override void Shoot()
+    {
+        Console.WriteLine("Ak Shoot!");
+        Console.WriteLine("伤害为" + atk);
+    }
+}
+public class M4 : Gun
+{
+    public M4()
+    {
+        atk = 98;
+    }
+    public override void Shoot()
+    {
+        Console.WriteLine("M4 Shoot!");
+        Console.WriteLine("伤害为" + atk);
+    }
+}
+
+public abstract class Decorator
+{
+    public abstract void SetComponet(Gun gun);
+}
+
+public class Buff : Decorator
+{
+    public override void SetComponet(Gun gun)
+    {
+        gun.atk *= 2;
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,14 @@
+﻿using Decorator;
+
+Ak ak=new Ak();
+M4 m4 = new M4();
+
+ak.Shoot();
+m4.Shoot();
+
+Buff buff = new Buff();
+buff.SetComponet(ak);
+buff.SetComponet(m4);
+
+ak.Shoot();
+m4.Shoot();
